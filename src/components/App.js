87d209e4cfd046648b0eb893f54468321f8cdc1b
Adrien Rousselet <adrien.rousselet@adotmob.com>
2020-05-14 17:15:27 +0200

    Update file architecture and bootstrap front app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './../styles/App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <h1>Pop cats</h1>
+    </div>
+  )
+}
+
+export default App
