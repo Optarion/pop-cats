d736e2dc1e9ae45be78f9a38220771a61d775848
Adrien Rousselet <adrien.rousselet@adotmob.com>
2020-05-14 18:19:30 +0200

    bootstrap ApolloClient and get cat's list from graphql's server

--- src/components/CatProductContainer.js
+++ src/components/CatProductContainer.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 import CatProduct from './CatProduct.js'
 
 function CatProductContainer (props) {
-  const cats = 
+  const CAT_QUERY = gql`
+    {
+      cats {
+        id
+        name
+        age
+        price
+      }
+    }
+  `
 
   return (
     <div className='cat-product-container'>
-      {
-        cats.map(cat => <CatProduct key={cat.id} cat={cat} />)
-      }
+      <Query query={CAT_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+
+          const cats = data.cats
+
+          return cats.map(cat => <CatProduct key={cat.id} cat={cat} />)
+        }}
+      </Query>
     </div>
   )
 }
