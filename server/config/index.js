62751bd1ca0bb0fc1ff83c2fdbf6d6088b024639
Adrien Rousselet <nibblerz@gmail.com>
2020-05-20 11:35:04 +0200

    Remove postgres uri from config file

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   postGres: {
-    uri: 'postgres://yguedlymadekec:b27a369c587983050c7974ca4aa4013a380d39fae277869bae1946be3593c8f8@ec2-34-230-149-169.compute-1.amazonaws.com:5432/dcc8it0vj6k79o'
+    uri: <postgres_heroku_uri>
   }
 }
